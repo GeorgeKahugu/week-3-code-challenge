8f2f25f5631987c8d74a52db6b10d2b6306c1e25
GeorgeKahugu <gee.kahugu@gmail.com>
2024-04-07 11:16:05 +0300

    initial commit

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 ## Description
 
-# Flatdango
+## Flatdango
 
 Flatiron Movie Theater is open for business! You will be building out an
 application, Flatdango, that allows a user to purchase movie tickets from the
--- challenge-3-flatadango/code-challenge-3/src/index.js
+++ challenge-3-flatadango/code-challenge-3/src/index.js
@@ -1,7 +1,7 @@
 // Your code here
 // Create addEventListener with DOM
 document.addEventListener('DOMContentLoaded',function(){
-    const baseURL="http://localhost:3000/films" 
+    const baseURL="http://localhost:3000"; 
 
 // Base URL for API
     fetch("http://localhost:3000/films/1")
@@ -15,21 +15,38 @@ document.addEventListener('DOMContentLoaded',function(){
     const filmslist=document.getElementById("films");
     let li=document.createElement("li");
     li.textContent=movie.title;
-    filmslist.appendChild("li");
+    filmslist.appendChild(li);
 
 
-// Selecting the Posters
+// Working on the Posters
 let poster=document.getElementById("poster")
 poster.src=movie.poster
 poster.alt=`${movie.title}Poster`;
 
-// Selecting the movie titles
+// Working on the movie titles
 let title=document.getElementById("title");
 title.textContent=movie.title
 
+// Selection of the runtime
+let runtime=document.getElementById("runtime");
+runtime.textContent=`Runtime: ${movie.runtime}minutes`;
 
+let ticket=document.getElementById("ticket-num");
+ticket.textContent=movie.ticket;
 
  }
+
+//  callout a new function fetching all the movie list based on the id
+function movielist(moviesId){
+    fetch(`${baseURL}/${moviesId}`)
+    .then((Response)=>Response.json())
+    .then((data)=>displayMovieList(data));
+}
+
+// Displaying the movielist on the page
+function displayMovieList(movies){
+
+}
 
 }); 
     
